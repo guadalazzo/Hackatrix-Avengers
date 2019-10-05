274df3911ca9968ad4647abe40930ba9bd498b3e
Guadalupe Jazmin Lazzo <guadalupelazzo@gmail.com>
2019-10-05 11:01:32 -0300

    wip map

---
+++ client/src/components/App/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import MapContainer from '../Map';
+
+
+import './styles.css'
+
+function App() {
+  return (
+    <div className="main-container">
+      <MapContainer  />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Map/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {Component} from 'react';
+import ReactMapGL from 'react-map-gl';
+import './styles.css';
+
+class Map extends Component {
+
+  state = {
+    viewport: {
+      width: '100vh',
+      height: '80vh',
+      latitude: -34.5780655,
+      longitude: -58.4560349,
+      zoom: 12
+    }
+  };
+
+  render() {
+    return (
+      <ReactMapGL
+        className="papa"
+        {...this.state.viewport}
+        onViewportChange={(viewport) => this.setState({viewport})}
+      />
+    );
+  }
+}
+export default Map;

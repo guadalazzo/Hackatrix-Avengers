fb0374c4badc581b4ab49cf074ffa724eb586367
lndgalante <lndgalante@gmail.com>
2019-10-05 13:57:09 -0300

    Fix sidebar

--- client/src/components/Sidebar/styled.js
+++ client/src/components/Sidebar/styled.js
@@ -2,13 +2,25 @@ import styled from 'styled-components';
 
 const Container = styled.div`
   width: 560px;
-
   height: 100vh;
+
+  z-index: 20;
   position: relative;
   padding: 90px 53px 0 80px;
 
+  ::after {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: rgba(0, 0, 0, 0.8);
+    z-index: -1;
+  }
+
   .info {
-    z-index: 2;
+    z-index: 20;
     color: white;
 
     .title {
@@ -22,6 +34,8 @@ const Container = styled.div`
       }
 
       h2 {
+        color: white;
+
         font-size: 48px;
         letter-spacing: 3px;
         margin-left: 12px;
@@ -31,17 +45,6 @@ const Container = styled.div`
 
     .filters {
     }
-  }
-
-  ::after {
-    content: '';
-    position: absolute;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    background-color: rgba(0, 0, 0, 0.8);
-    z-index: -1;
   }
 `;
 

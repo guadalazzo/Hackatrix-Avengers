4948259cda485787dba8e65d0e61f883c824024f
Guadalupe Jazmin Lazzo <guadalupelazzo@gmail.com>
2019-10-05 11:59:16 -0300

    added overlaay to component

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -7,7 +7,7 @@ import './styles.scss'
 function App() {
   return (
     <div className="main-container">
-      <MapContainer  />
+      <MapContainer overlay={false} />
     </div>
   );
 }
--- client/src/components/Map/index.js
+++ client/src/components/Map/index.js
@@ -4,30 +4,29 @@ import ReactMapGL from 'react-map-gl';
 import './styles.scss';
 
 class Map extends Component {
-
-  state = {
-    viewport: {
-      width: '100vh',
-      height: '80vh',
-      latitude: -34.5780655,
-      longitude: -58.4560349,
-      zoom: 12,
-      light: {
-        anchor: 'viewport',
-        color: "red",
-        intensity: 0.4
-      }
-    },
-   
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      viewport: {
+        width: '100vh',
+        height: '80vh',
+        latitude: -34.5780655,
+        longitude: -58.4560349,
+        zoom: 12,
+      },
+    };
+  }
 
   render() {
     return (
-      <ReactMapGL
-        mapStyle="mapbox://styles/mapbox/streets-v9"
-        {...this.state.viewport}
-        onViewportChange={(viewport) => this.setState({viewport})}
-      />
+      <React.Fragment>
+      <span className={this.props.overlay ? 'overlay':''} ></span> 
+        <ReactMapGL
+          mapStyle="mapbox://styles/mapbox/streets-v9"
+          {...this.state.viewport}
+          onViewportChange={(viewport) => this.setState({viewport})}
+        />
+       </React.Fragment>
     );
   }
 }
